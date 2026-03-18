b510a6cb28285f4de35794b6336667f63b75776b
sele906 <seunga906@gmail.com>
2026-03-18 23:10:27 +0900

    input 테스트

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -64,7 +64,7 @@ function Header() {
                             onChange={(e) => setSearchValue(e.target.value)}
                             onKeyDown={handleSearchSubmit}
                             autoFocus
-                            autoComplete="tickwirj"
+                            autoComplete="off"
                             autoCorrect="off"
                             autoCapitalize="off"
                             spellCheck="false"
